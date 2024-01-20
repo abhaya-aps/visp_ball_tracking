7809e64f1004bbee02ca24443af78119fe1bd6e0
Dmytro Romanov <dmytro.romanov@nmbu.no>
2024-01-21 00:32:40 +0100

    Successfull build of gui_test.cpp

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Visual tracking of objects using Realsense D410 camera and UR10 robot.
 
+# GUI
+
+The project will most likely use [ImGUI](https://github.com/ocornut/imgui) as Realsense seems to depend on it.
+
 # Installation steps
 
 1. Install [VISP](https://visp-doc.inria.fr/doxygen/visp-daily/tutorial-install-ubuntu.html) as described in the documentation.
@@ -9,6 +13,18 @@ Visual tracking of objects using Realsense D410 camera and UR10 robot.
 3. Proceed with RealSense library installation using vcpkg as described [here](https://github.com/IntelRealSense/librealsense).
 4. ```cmake . && make```
 
+# Installing libraries
+
+```
+cd vcpkg
+./vcpkg install imgui
+sudo apt update
+sudo apt install libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev pkg-config libglfw3 libglfw3-dev
+./vcpkg install glfw3
+./vcpkg install opengl
+./vcpkg integrate install
+
+```
 
 # Downloading sample *.bag files
 
--- simulation.cpp
+++ simulation.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 //#include <opencv2/opencv.hpp>
 #include <librealsense2/rs.hpp>
+#include <imgui.h>
+//#include "imgui_impl_glfw.h"
 
+#ifndef PLAYBACK_ABS_FILEPATH
+    #define PLAYBACK_ABS_FILEPATH "error" //this one is defined in CmakeLists
+#endif
 // Function to estimate distance (in m) based on the depth information
 double estimateDistance(const rs2::depth_frame& depth_frame, int cx, int cy) {
     // Get the depth value at the specified pixel
@@ -12,7 +17,12 @@ double estimateDistance(const rs2::depth_frame& depth_frame, int cx, int cy) {
 
 int main() {
     // Initialize the RealSense pipeline
-    
+    ImGui::Text("Hello, world %d", 123);
+if (ImGui::Button("Save"))
+    MySaveFunction();
+ImGui::InputText("string", buf, IM_ARRAYSIZE(buf));
+ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+
     rs2::pipeline pipe;
     rs2::config cfg;
     //make sure to download the file using ./download_sample_bag_files.sh script
